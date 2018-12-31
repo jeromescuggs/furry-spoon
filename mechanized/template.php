c2e861dda333cef81b5e0a48c4d36cf0d7c2157d
jerome scuggs <subspacer@gmail.com>
2018-12-31 09:48:48 -0600

    initial commit, created template.php+file structure

---
+++ mechanized/template.php
@@ -0,0 +1,13 @@
+<!--
+
+content goes here,
+
+if your lazy ass ever gets around to it
+
+because let's be real here
+
+you're probably gonna reformat before you ever touch this file
+
+you procastinating sonofabitch
+
+-->
